46e8a362551a160fdd471723277883b6583783a9
WiFuchs <willfuchs13@gmail.com>
2015-02-27 11:25:35 -0800

    Update dynamicFunctions.js

--- dynamicFunctions.js
+++ dynamicFunctions.js
@@ -1,5 +1,7 @@
 var markingMode = false;
-
+var canvas = document.getElementbyID("Minesweeper");
+	canvas.width = window.innerWidth;
+	canvas.height = window.innerHeight;
 var setMarkingMode = function(){
 	markingMode = !markingMode;
 }
