39e39be5c96b2e9567d64f02d1d1d524fb17d4d8
WiFuchs <willfuchs13@gmail.com>
2015-02-28 14:00:04 -0800

    Update dynamicFunctions.js

--- dynamicFunctions.js
+++ dynamicFunctions.js
@@ -1,20 +1,4 @@
 var markingMode = false;
-var canvas = document.getElementbyID("Minesweeper");
-
-//return an obect with the dimension of the viewport of the browser
-function getViewportDimension() {
-var e = window, a = 'inner';
-if (!( 'innerWidth' in window )) {
-    a = 'client';
-    e = document.documentElement || document.body;
-}
-return {w:e[a + 'Width'], h:e[a + 'Height']};
-}
-
-//apply the dimension to the canvas
-var dim = getViewportDimension();
-canvas.width = dim.w;
-canvas.height = dim.h;
 
 //toggle marking mode, used in Minesweeper.pde
 var setMarkingMode = function(){
